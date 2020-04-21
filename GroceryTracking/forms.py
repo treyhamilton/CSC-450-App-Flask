cceffa5e0a78f41f77511a2439df9eac9c410d84
Paul-Witt <Paulwitt9617@gmail.com>
2020-04-20 23:05:08 -0400

    Pached "edit account" bug

--- GroceryTracking/forms.py
+++ GroceryTracking/forms.py
@@ -62,12 +62,12 @@ class EditAccountForm(FlaskForm):
     submit = SubmitField("Submit")
 
     def validate_username(self, username):
-        user = User.query.filter_by(username=username.data).first()
+        user = User.query.filter_by(username=username).first()
         if user:
             raise ValidationError('That username is taken. Please choose a different one.')
 
     def validate_email(self, email):
-        user = User.query.filter_by(email=email.data).first()
+        user = User.query.filter_by(email=email).first()
         if user:
             raise ValidationError('That email is taken. Please choose a different one.')
 
